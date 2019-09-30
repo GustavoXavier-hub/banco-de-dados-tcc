b0cf0f8d7bfc8ee54310f0b1cee4b5736c691c38
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-09-30 18:48:05 -0300

    BANCO DE DADOS-TCC-VERSAO12

--- SCRIPT TCC DO BANCO.sql
+++ SCRIPT TCC DO BANCO.sql
@@ -2,7 +2,7 @@ create database FAST_201902
 
 /*criacao da tabela cliente */
 create table cliente
-(id_clie int identity not null primary key,
+(id_clie int  not null primary key,
 nm_clie  varchar(45) not null,
 dat_nas_clie datetime not null,
 tele_cont_clie numeric(7,5) not null,
@@ -12,25 +12,25 @@ go
 
 /*criacao da tabela tp_pgto */
 create table tp_pgto
-(id_tp_pgto int identity not null primary key,
+(id_tp_pgto int  not null primary key,
 dsc_tp_pgto varchar(15));  
   
 go
 /*criacao da tabela tp_serv */
 create table tp_serv
-(id__tp_serv int identity not null primary key,
+(id__tp_serv int  not null primary key,
 dsc_tp_serv varchar(15)); 
 
 go
 /*criacao da tabela tp_pessoa */
 create table tp_pessoa
-(id_tp_pessoa int identity not null primary key,
+(id_tp_pessoa int  not null primary key,
 dsc_tp_pessoa varchar(15)); 
 
 go
 /*criacao da tabela empresa */
 create table empresa
-(id_emp int identity not null primary key,
+(id_emp int  not null primary key,
 nm_emp varchar(20) not null,
 cnpj_emp numeric(7,5)   not null);
 
@@ -38,19 +38,19 @@ go
 
 /*criacao da tabela pessoa */
 create table pessoa 
-(id_func int identity not null primary key,
+(id_func int  not null primary key,
 id_tp_pessoa int constraint tp_pessoa_id_fk02 references tp_pessoa (id_tp_pessoa),
 id_emp  int  not null  constraint empresa_id_fk01   references empresa (id_emp),
 nm_func varchar(20) not null,
 dat_nas_func date not null,
-rg_func numeric (7,5),
-cpf_func numeric (8,8),
-cp_func  numeric (6,0));
+rg_func varchar (40),
+cpf_func varchar (40),
+cp_func  numeric (12));
 go
 
 /*criacao da tabela prestador */
 create table prestador
-(id_prestador int identity not null primary key,
+(id_prestador int  not null primary key,
 id_func int constraint pessoa_id_fk03 references pessoa (id_func),
 cnh_prestador numeric (7,2));
  
@@ -58,8 +58,8 @@ cnh_prestador numeric (7,2));
 
  /*criacao da tabela veiculo */
  create table veiculo
- (id_ve int identity not null primary key,
- id_prestador int constraint prestador_id_fk04 references prestador (id_prestador),
+ (id_ve int  not null primary key,
+ id_prestador int  constraint prestador_id_fk04 references prestador (id_prestador),
  pl_ve numeric (7,5) not null,
  cor_ve varchar (20) not null,
  marca_ve varchar(20) not null,
@@ -78,18 +78,31 @@ cnh_prestador numeric (7,2));
  id_tp_pgto int constraint tp_pgto_fk06 references tp_pgto (id_tp_pgto),
  id_clie int constraint cliente_fk07 references cliente (id_clie),
  id_prestador int constraint prestador_id_fk08 references prestador (id_prestador),
- id_contrato_Serv  int identity not null, 
+ id_func int constraint pessoa_id_fk05 references pessoa (id_func),
+ id_contrato_Serv  int  not null, 
  cubagem_serv numeric (7,4) not null,
  dat_serv datetime not null,
  vl_pgto numeric (10,5)not null );
  go
  
- /*alter table da tabela contrato_serviço
+ /*alter table da tabela contrato_serviço*/
 
- alter table contrato_serviço
- add  id__tp_serv  int  not null 
-
+ ALTER TABLE contrato_serviço alter COLUMN id__tp_serv int;
+ go
+ alter table veiculo alter COLUMN id_ve int ;
  
+ alter table prestador alter COLUMN id_prestador int ;
+ 
+  alter table pessoa alter COLUMN id_func int ;
+ 
+  alter table empresa alter COLUMN id_emp int ;
+
+  alter table tp_pessoa alter COLUMN id_tp_pessoa int ;
+
+   alter table tp_pgto alter COLUMN id_tp_pgto int ;
+
+   alter table cliente alter COLUMN id_clie int ;
+/*
  alter table contrato_serviço
  add  id_tp_pgto   int  not null 
 
@@ -129,48 +142,59 @@ ADD CONSTRAINT con_serv PRIMARY KEY (id_contrato_Serv)
 
  /* insert da tabela cliente */
 
-  insert into cliente(nm_clie,dat_nas_clie,tele_cont_clie,end_clie,sex_clie)
-       values ('Carlos Roberto','2018/06/12 18:00:00',0, 'parque edu chaves','Masculino');
-	          
+  insert into cliente(id_clie,nm_clie,dat_nas_clie,tele_cont_clie,end_clie,sex_clie)
+       values (1,'Carlos Roberto','2018/06/12 18:00:00',1, 'parque edu chaves','Masculino');
+go	          
  /* insert da tabela tp_pgto */
 
- insert into tp_pgto  (dsc_tp_pgto)
-values ( 'Debito'),
-       ('Credito');
-
+ insert into tp_pgto  (id_tp_pgto,dsc_tp_pgto)
+values ( 1,'Debito'),
+       (2,'Credito');
+go
 	          
- /* insert da tabela tp_pessoa */
-insert into tp_pessoa (dsc_tp_pessoa)
-values ('Juridica'),
-       ('fisica');	   
+ /* insert da tabela tp_serv */
+insert into tp_serv (id__tp_serv,dsc_tp_serv)
+values (1,'mudanca'),
+       (2,'locacao');	   
+go
+
+/* insert da tabela tp_pessoa */
+insert into tp_pessoa (id_tp_pessoa,dsc_tp_pessoa)
+values (1,'Juridica'),
+       (2,'fisica');	   
+go
+
 
  /* insert  da tabela empresa */
 
- insert into empresa (nm_emp,cnpj_emp)
- values ('sdadsasdsa',8);
+ insert into empresa (id_emp,nm_emp,cnpj_emp)
+ values (1,'sdadsasdsa',9);
 
- 
+ go
  /* insert  da tabela pessoa */
 
- insert into pessoa (nm_func,dat_nas_func,cpf_func,cp_func,id_emp,id_tp_pessoa,rg_func)
- values ('carlos','2018/06/12 18:00:00',0,1,2,3,4);
-
+ insert into pessoa (id_func,id_tp_pessoa,id_emp,nm_func,dat_nas_func,rg_func,cpf_func,cp_func)
+ values  (1,1,1,'carlos','2018/06/12 18:00:00',2,2,4)
+ (/*1,1,'carlos','2018/06/12 18:00:00',00,01,02*/);
+ go
 /* insert da tabela prestador */
 
-insert into prestador (cnh_prestador)
-values (0)
- 
+insert into prestador (id_prestador,id_func,cnh_prestador)
+values (8,1,0)
+ go
  /* insert da tabela veiculo */
 
- insert into  veiculo (pl_ve,cor_ve,marca_ve,doc_ve,modelo_ve,ano_ve,km_ve,status_ve,rastreador_ve,cod_rastreador_ve)
- values (01,'azul','honda',02,'Conversivel','2018/06/12 18:00:00',03,'Ativo','Desligado',04)
-
+ insert into  veiculo (id_ve,id_prestador,pl_ve,cor_ve,marca_ve,doc_ve,modelo_ve,ano_ve,km_ve,status_ve,rastreador_ve,cod_rastreador_ve)
+ values (1,8,1,'azul','Conversivel',2,'Conversivel','2018/06/12 18:00:00',03,'Desligado','Desligado',06);
+ /* (05,8,1,'azul','honda','Conversivel','2018/06/12 18:00:00',03,'Ativo','Desligado',06*/
+ go
  /* insert da tabela contrato_serviço */
 
- insert into contrato_servico (cubagem_serv,dat_serv,vl_pgto)
-values ()
+ insert into contrato_servico (id__tp_serv,id_tp_pgto,id_clie,id_prestador,id_func, id_contrato_Serv,cubagem_serv,dat_serv,vl_pgto)
+values 
+(1,1,1,8,1,1,1,'2018/06/12 18:00:00',4);
 
-
+go
 /* drop das tabelas */
 drop table cliente
 
@@ -196,3 +220,7 @@ drop table contrato_serviço
 172.16.48.10
 Usuário: fastmoving
 Senha: m0v1ngf@st */
+
+
+sp_help contrato_servico id_func
+
